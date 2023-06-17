1bd7586c9eb1ba85488b35fade6c6d0dcb8961a9
Protozx <83438783+Protozx@users.noreply.github.com>
2023-06-17 07:51:31 -0500

    sssd
    sssd

--- src/main/resources/static/trabajador_social/js/ts_unir_grupoinfanteC.js
+++ src/main/resources/static/trabajador_social/js/ts_unir_grupoinfanteC.js
@@ -1,19 +1,69 @@
-$(document).ready(function () {
-  alert("Everything is fine");
+// Espera a que el documento HTML esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
 
-  var nombreUsuario = "ALEJANDRO ZÁRATE CÁRDENAS";
-  var grupos = [{ "id": "1020", "nombre": "2BM1" }, { "id": "1030", "nombre": "2BM2" }, { "id": "1040", "nombre": "2BM3" }]
+    // Define los datos y configuraciones por defecto
+    const menuPlaceholder = "Pan de granola con leche y ensalada cesar";
+    const infantes = [{
+        "id": 2017200325, "nombre": "Melanie Zavala Tinoco",
+    }, {
+        "id": 2017200326, "nombre": "Juan Carlos García López"
+    }, {
+        "id": 2017200327, "nombre": "Ana María Rodríguez Torres"
+    }, {
+        "id": 2017200328, "nombre": "Carlos Hernández Sánchez"
+    }, {
+        "id": 2017200329, "nombre": "María Fernández García"
+    }, {
+        "id": 2017200330, "nombre": "Javier Torres Méndez"
+    }, {
+        "id": 2017200331, "nombre": "Laura Martínez Ramírez"
+    }, {
+        "id": 2017200325, "nombre": "Melanie Zavala Tinoco",
+    }, {
+        "id": 2017200326, "nombre": "Juan Carlos García López"
+    }, {
+        "id": 2017200327, "nombre": "Ana María Rodríguez Torres"
+    }, {
+        "id": 2017200328, "nombre": "Carlos Hernández Sánchez"
+    }, {
+        "id": 2017200332, "nombre": "Luis López Torres"
+    }];
 
+    // Función que crea un nuevo menú
 
+    // Función que crea un nuevo select de cantidad
+    const createGrupos = (id) => {
+        const cantidadD = document.createElement("select");
+        cantidadD.classList.add("form-select");
+        cantidadD.setAttribute("id", "cf" + id);
 
+        const options = ["3BM1", "2BV3", "1SC3"];
+        options.forEach(optionText => {
+            const option = document.createElement("option");
+            option.text = optionText;
+            cantidadD.appendChild(option);
+        });
 
+        return cantidadD;
+    }
 
+    // Función que crea una nueva fila de infante
+    const createInfanteRow = (infante) => {
+        const row = document.createElement("tr");
+        row.setAttribute("id", "idf");
 
+        row.appendChild(document.createElement("td")).textContent = infante.id;
+        row.appendChild(document.createElement("td")).textContent = infante.nombre;
+        row.appendChild(document.createElement("td")).appendChild(createGrupos(infante.id));
 
-  $("#nombreusuarioprint").text(nombreUsuario.toUpperCase());
-  
-  $.each(grupos, function (key, grupo) {
-    var opt = $("<option>").val(grupo.id).text(grupo.nombre);
-    $("#idgf").append(opt);
-  });
+        return row;
+    }
+
+    // Recorre cada infante y crea su respectiva fila
+    infantes.forEach(infante => {
+        const infantesPrintElement = document.getElementById("infantesprint");
+        const tbodyElement = infantesPrintElement.querySelector("tbody");
+        const newRow = createInfanteRow(infante);
+        tbodyElement.appendChild(newRow);
+    });
 });
